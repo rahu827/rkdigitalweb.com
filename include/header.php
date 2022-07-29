1975cb9a17aee150a51083f2c72346833d24df5e
rahu827 <84239243+rahu827@users.noreply.github.com>
2022-07-29 22:12:42 +0530

    initialize

---
+++ include/header.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg">
+  <div class="container" id="navcst">
+  <a class="navbar-brand" href="#"><span>R</span>k Digital Web</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
+        </li>
+      </ul>
+      <div class="d-flex" style="align-items: center;gap: 1rem;">
+        <a href="#" class="btn_custom">Get In Touch</a>
+      <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Web </title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/resp.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include 'include/header.php' ?>
+    <section id="header_cst">
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8" style="position: relative;">
+                        <div class="col_cst">
+                            <span>Web Designing & DevelopMent</span>
+                            <h1>Full Service Digital Marketing</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi explicabo numquam, similique, doloribus aliquam debitis dicta quos animi</p>
+                            <a href="#" class="btn_custom" style="background-color: #fff; color:#000;">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </section>
+    <div class="wbimage">
+        <img src="img/des.png" alt="website template" class="img-fluid">
+    </div>
+    <div class="wbimage" style="left: 0;">
+        <img src="img/gra.png" alt="website template" class="img-fluid">
+    </div>
+    <section id="service">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-7">
+                    <span>Our Services</span>
+                    <h1>We provide Our Client Best And Flexible website Design</h1>
+                    <p class="mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, animi nam quibusdam quas ad quam. Et quos exercitationem quis harum eius ullam aut iure possimus temporibus pariatur? Molestias, porro accusantium.</p>
+                    <a href="#" class="btn_custom">All Services</a>
+                </div>
+                <div class="col-lg-5 maindiv mt-5">
+                    <div class="innerdiv">
+                        <img src="img/i1.png" alt="icon1" class="img-fluid" width="100px">
+                        <h2>UI design.</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, labore esse totam porro, minima deleniti qui</p>
+                        <a href="#" class="btn_custom">Learn</a>
+                    </div>
+                </div>
+                <div class="col-lg-5 maindiv mt-5">
+                    <div class="innerdiv">
+                        <img src="img/i1.png" alt="icon1" class="img-fluid" width="100px">
+                        <h2>Website design</h2>
+                        <p>Website designing is a very important base step in starting a successful project. A website is an outline of the services and products...</p>
+                        <a href="#" class="btn_custom">Learn</a>
+                    </div>
+                </div>
+                <div class="col-lg-5 maindiv mt-5">
+                    <div class="innerdiv">
+                        <img src="img/i1.png" alt="icon1" class="img-fluid" width="100px">
+                        <h2>Web DevelopMent.</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, labore esse totam porro, minima deleniti qui</p>
+                        <a href="#" class="btn_custom">Learn</a>
+                    </div>
+                </div>
+                <div class="col-lg-5 maindiv mt-5">
+                    <div class="innerdiv">
+                        <img src="img/i1.png" alt="icon1" class="img-fluid" width="100px">
+                        <h2>SEO</h2>
+                        <p>Rk Digital Web is a team of Internet Marketing wizards who can magically deliver you a profound ROI based campaign served</p>
+                        <a href="#" class="btn_custom">Learn</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="responsive" class="responsive_1">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-8">
+                    <img src="img/img1.png" class="img-fluid">
+                </div>
+                <div class="col-lg-4">
+                    <h2>We Provide our client a professional website & Mobile responsive</h2>
+                    <p class="mb-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo minima exercitationem in ipsum. Consequuntur perspiciatis velit ea odit veniam consectetur culpa.</p>
+                    <a href="#" class="btn_custom" style="background-color: #fff; color:blue;">Get In Touch</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="work">
+        <div class="container mt-5">
+            <div class="row">
+
+                <div class="col-lg-4 mt-5">
+                    <span><i class="fa-solid fa-laptop-file me-2"></i>Recently Work</span>
+                    <h2>Our Recent Work</h2>
+                    <ul>
+                        <li>Brand Identity</li>
+                        <li>Website Design & Development</li>
+                        <li>SEO & Analytics</li>
+                        <li>Social Media Generation</li>
+                    </ul>
+                    <a href="#" class="btn_custom">View Project <span style="background: none;" class="arrow"><i class="fa-solid fa-arrow-right""></i></span></a>
+                </div>
+                <div class=" col-lg-8">
+                                <img src="img/img2.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="owner">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <span>About Us</span>
+                    <!-- <h2 class="text-uppercase"></h2> -->
+                    <h1 class="mt-3">We are 3 Year In Bussiness</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quidem aperiam quia harum veniam reiciendis culpa voluptates ducimus, velit at Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, quidem aperiam quia harum veniam reiciendis culpa voluptates ducimus, velit at</p>
+                    <div class="about">
+                        <div class="about_contetn">
+                            <em style="color:blue;"><i class="fa-solid fa-business-time me-3"></i>Founder</em>
+                            <h3><i class="fa-solid fa-user" style="color: blue;"></i> Mr.Rahul</h3>
+                        </div>
+                        <div class="ex">
+                            <p>3+</p>
+                            <em>experience</em>
+                        </div>
+
+                    </div>
+
+                    <div class="skill mb-4">
+
+                        <div class="ul1">
+                            <h4><i class="fa-solid fa-spa me-2" style="color: blue;"></i>Skills</h4>
+                            <ul>
+                                <li><i class="fa-solid fa-check me-2" style="color: blue;"></i>web Designing & Development</li>
+                                <li><i class="fa-solid fa-check me-2" style="color: blue;"></i>Graphic Designer</li>
+                            </ul>
+                        </div>
+                        <div class="ul1">
+                            <h4><i class="fa-solid fa-spa me-2" style="color: blue;"></i>Skills</h4>
+                            <ul>
+                                <li><i class="fa-solid fa-check me-2" style="color: blue;"></i>React App</li>
+                                <li><i class="fa-solid fa-check me-2" style="color: blue;"></i>Wordpress Website</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="#" class="btn_custom">Know More</a>
+                </div>
+                <div class="col-lg-6">
+                    <img src="img/owner.png" alt="owner image" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="divider">
+        <div class="container-fluid">
+            <div class="row">
+                <h3>Ready To Make a Real Change? Let’s Build this Thing Together!</h3>
+                <p>If You Trust Us Then Contact Us</p>
+            </div>
+        </div>
+    </div>
+    <section id="get_in_touch">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h2 class="mb-4"><i class="fa-brands fa-joget me-2"></i>Get In Touch</h2>
+                        <form action="" method="post">
+                            <div class="mb-2">
+                                <input type="text" class="form-control" placeholder="Full Name">
+                            </div>
+                            <div class="mb-2">
+                                <input type="text" class="form-control" placeholder="Email Id">
+                            </div>
+                            <div class="mb-2">
+                                <input type="text" class="form-control" placeholder="Mobile No:">
+                            </div>
+                            <div class="mb-4">
+                                <textarea name="" class="form-control" id="" cols="5" rows="2" placeholder="Say Something"></textarea>
+                            </div>
+                            <a href="#" class="btn_custom">Send <i class="fa-solid fa-paper-plane"></i></a>
+                        </form>
+                    </div>
+                    <div class="col-lg-6">
+                        <h2 class="mb-4"><i class="fa-solid fa-house-chimney-window me-3"></i>Contact Information</h2>
+                        <h4>Mobile No:</h4>
+                        <p><i class="fa-solid fa-phone me-3"></i>+91-8732034337</p>
+                        <h4>Mail Address</h4>
+                        <p><i class="fa-solid fa-envelope me-3"></i>rkdesigner@gmail.com</p>
+                        <ul class="social_media">
+                            <li><i class="fa-brands fa-facebook"></i></li>
+                            <li><i class="fa-brands fa-instagram"></i></li>
+                            <li><i class="fa-brands fa-twitter"></i></li>
+                            <li><i class="fa-brands fa-google-plus-g"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <section id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="div-footer">
+                        <h2>Join The Most Enthusiastic And Empowered WordPress Community On The Web</h2>
+                        <p>Complete Design Control Over Everything</p>
+                        <a href="#" class="btn_custom" style="background-color: #fff; color:blue;">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="div_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="footer_nav">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">About US</a></li>
+                            <li><a href="">Portfolio</a></li>
+                            <li><a href="">Term</a></li>
+                            <li><a href="">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="footer_nav">
+                        <ul>
+                            <li><a href=""><i class="fa-brands fa-facebook"></i></a></li>
+                            <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fa-brands fa-google-plus-g"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
